b2b8a8d27e731948c70162048234590c1275ab89
tfajoux <thomas_fajoux@ext.connect-tech.sncf>
2023-11-22 20:11:07 +0100

    ajustement code pour REX

--- main.js
+++ main.js
@@ -2,109 +2,117 @@ const { app, BrowserWindow, dialog, ipcMain, Notification, shell } = require("el
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const createWindow = () => {
-	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		// Permet d'utiliser le ipcMain
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-		},
-	});
-
-	win.loadFile("index.html");
-	// Activation de la console de debug uniquement en dev
-	if (!app.isPackaged) {
-		win.webContents.openDevTools({
-			mode: "right",
-		});
-	}
-};
-
 app.whenReady().then(() => {
-	createWindow();
+  createWindow();
 });
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // Permet d'utiliser le ipcMain
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("index.html");
+  // Activation de la console de debug uniquement en dev
+  if (!app.isPackaged) {
+    win.webContents.openDevTools({
+      mode: "right",
+    });
+  }
+};
 
 // Système de reload de l'application après un changement dans le code
 if (!app.isPackaged) {
-	try {
-	  require("electron-reloader")(module, {
-		debug: true,
-		watchRenderer: true,
-	  });
-	} catch (_) {
-	  console.log("Error !");
-	}
+  try {
+    require("electron-reloader")(module, {
+      debug: true,
+      watchRenderer: true,
+    });
+  } catch (_) {
+    console.log("Error !");
   }
+}
 
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") app.quit();
 });
 
 // Déclenche l'action quand le renderer.js émet l'événement "place-disponible"
-ipcMain.on("place-disponible", (_, url, numeroEpreuve, navigateurActivee) => {
-	if(navigateurActivee) {
-		ouvrirDansLeNavigateur(url);
-	}
-	envoiMail(url, numeroEpreuve);
+ipcMain.on("place-disponible", (_, url, concours, numeroEpreuve, navigateurActivee, notificationActivee, envoiMailActive) => {
+  if (navigateurActivee) {
+    ouvrirDansLeNavigateur(url);
+  }
+
+  if (notificationActivee) {
+    afficherNotificationPlace(url, concours, numeroEpreuve);
+  }
+
+  if (envoiMailActive) {
+    envoiMail(url, numeroEpreuve);
+  }
+
 });
 
 ipcMain.on("ouvrir-avertissement", (_, message) => {
-	ouvrirFenetreAvertissement(message);
+  ouvrirFenetreAvertissement(message);
 });
 
-ipcMain.on("notification-bureau", (_, concours, numeroEpreuveSouhaitee, url) => {
-	const NOTIFICATION_TITLE = `Concours Poney ${concours}`;
-    const NOTIFICATION_BODY = `Place disponible pour l'épreuve ${numeroEpreuveSouhaitee}`;
-    const notification = new Notification({
-		title: NOTIFICATION_TITLE, 
-		body: NOTIFICATION_BODY, 
-		icon: "./poney.ico" 
-	});
+function afficherNotificationPlace(url, concours, numeroEpreuveSouhaitee) {
+  const NOTIFICATION_TITLE = `Concours Poney ${concours}`;
+  const NOTIFICATION_BODY = `Place disponible pour l'épreuve ${numeroEpreuveSouhaitee}`;
+  const notification = new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+    icon: "./poney.ico"
+  });
 
-	notification.show();
+  notification.show();
 
-    notification.on("click",(_) => {
-        ouvrirDansLeNavigateur(url);
-    });
-})
+  notification.on("click", (_) => {
+    ouvrirDansLeNavigateur(url);
+  });
+}
 
 function envoiMail(url, numeroEpreuve) {
-	if(process.env.DESTINATAIRE == undefined) {
-		ouvrirFenetreAvertissement("Aucun destinataire n'a été défini.")
-		return;
-	}
-	const transporter = nodemailer.createTransport({
-		service: "gmail",
-		auth: {
-			user: process.env.USER,
-			pass: process.env.TOKEN,
-		},
-	});
+  if (process.env.DESTINATAIRE == undefined) {
+    ouvrirFenetreAvertissement("Aucun destinataire n'a été défini.")
+    return;
+  }
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.USER,
+      pass: process.env.TOKEN,
+    },
+  });
 
-	const mailOptions = {
-		from: process.env.EMETTEUR,
-		to: process.env.DESTINATAIRE,
-		subject: `Concours Poney - Épreuve ${numeroEpreuve}`,
-		text: `Place de libre pour le concours ${url}`,
-	};
+  const mailOptions = {
+    from: process.env.EMETTEUR,
+    to: process.env.DESTINATAIRE,
+    subject: `Concours Poney - Épreuve ${numeroEpreuve}`,
+    text: `Place de libre pour le concours ${url}`,
+  };
 
-	transporter.sendMail(mailOptions, function (err, info) {
-		if (err) console.log(err);
-		else console.log(info);
-	});
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
 }
 
 function ouvrirFenetreAvertissement(message) {
-	dialog.showMessageBox({
-		type: "warning",
-		buttons: [],
-		title: "Avertissement",
-		message,
-	});
+  dialog.showMessageBox({
+    type: "warning",
+    buttons: [],
+    title: "Avertissement",
+    message,
+  });
 }
 
 function ouvrirDansLeNavigateur(url) {
-	shell.openExternal(url); // Ouvre l'url dans un navigateur
+  shell.openExternal(url); // Ouvre l'url dans un navigateur
 }
--- renderer.js
+++ renderer.js
@@ -23,33 +23,33 @@ async function afficherEpreuves(concours, epreuvesSouhaitees) {
 
     const url = `https://ffecompet.ffe.com/concours/${concours.trim()}/programme`
 
-	const documentConcours = await recuperationInformationsPageConcours(url);
+    const documentConcours = await recuperationInformationsPageConcours(url);
 
     // "tr[class=cv_liste_ligne_0]" est l'élément de la page HTML contenant les informations d'une épreuve.
     // La fonction permet de récupérer tous les éléments des épreuves sous la forme d'un tableau (de 'NodeList')
     const epreuvesConcours = documentConcours.body.querySelectorAll("tr[class=cv_liste_ligne_0]");
-    
+
     // Affichage uniquement des épreuves renseignées dans le formulaire
     for (numeroEpreuveSouhaitee of epreuvesSouhaitees) {
         // Le premier élément du tableau étant à 0, on doit décrément de 1 pour tomber sur l'épreuve désirée.
         // Ex. : l'épreuve souhaitée est la 6. On veut donc dans le tableau des épreuves globales l'élément qui est au 5ième rang.
         // L'information sur les places restantes à l'épreuve sont ensuite dans l'élément children à l'index 4.
         let epreuve = epreuvesConcours[parseInt(numeroEpreuveSouhaitee, 10) - 1];
-        if(epreuve == null || epreuve == undefined) {
+        if (epreuve == null || epreuve == undefined) {
             arreterRequete();
             ouvrirFenetreAvertissement("Etes-vous sûr de l'épreuve renseignée ?");
             return;
         }
-        
+
         allumerLed();
 
         const placesRestantes = epreuve.children[4].innerHTML === '-' ? 0 : epreuve.children[4].innerHTML;
 
         // Création d'une div contenant les informations de l'épreuve que l'on va ensuite ajouter à l'élément 'infos-epreuves-concours' afin de l'afficher à l'écran
         afficherPlacesConcours(document, numeroEpreuveSouhaitee, placesRestantes);
-        
 
-        if(!isNaN(parseInt(placesRestantes)) 
+
+        if (!isNaN(parseInt(placesRestantes))
             && parseInt(placesRestantes) !== 0
         ) {
             const nomConcours = documentConcours.getElementsByClassName("boite TEnteteConcours")[0].getElementsByClassName("top")[0].getElementsByClassName("left")[0].innerText.split('- ').pop()
@@ -60,29 +60,21 @@ async function afficherEpreuves(concours, epreuvesSouhaitees) {
 
             const envoiMailActive = document.querySelector('#envoiMail').checked;
 
-			ouvrirFenetreAvertissement(`Place disponible pour l'épreuve ${numeroEpreuveSouhaitee}`);
+            ouvrirFenetreAvertissement(`Place disponible pour l'épreuve ${numeroEpreuveSouhaitee}`);
 
-            if (envoiMailActive) {
-                ipcRenderer.send("place-disponible", url, numeroEpreuveSouhaitee, navigateurActivee); // Permet d'envoyer un 'event' avec un contenu au `main.js` afin de déclencher l'action d'envoi de mail
-            }
-
-            if (notificationActivee) {
-                afficherNotificationPlace(nomConcours, numeroEpreuveSouhaitee, url);
-            }
+             // Permet d'envoyer un 'event' avec un contenu au `main.js` afin de déclencher des actions par le processus principal 
+            ipcRenderer.send("place-disponible", url, concours, numeroEpreuveSouhaitee, navigateurActivee, notificationActivee, envoiMailActive);
 
             arreterRequete();
         }
     }
-
 }
 
 /**
  * Appel au site de la FFE pour récupérer le document fourni par la ressource et le met en forme
- * @param {*} concours 
- * @returns 
  */
 async function recuperationInformationsPageConcours(url) {
-	// appel de la ressource (lien du concours)
+    // appel de la ressource (lien du concours)
     const response = await fetch(url); // L'appel nous ramène un document
 
     if (response.url !== url) {
@@ -98,13 +90,10 @@ async function recuperationInformationsPageConcours(url) {
 }
 
 function ouvrirFenetreAvertissement(message) {
-	ipcRenderer.send("ouvrir-avertissement", message);
+    ipcRenderer.send("ouvrir-avertissement", message);
 }
 
-// Permet l'affichage d'une notification OS
-function afficherNotificationPlace(concours, numeroEpreuveSouhaitee, url) {
-    ipcRenderer.send("notification-bureau", concours, numeroEpreuveSouhaitee, url);
-}
+
 function allumerLed() {
     document.getElementById('led-red').style.display = 'none';
     document.getElementById('led-green').style.display = 'block';
